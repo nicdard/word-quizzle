8db3c00f99f9aea9479668dc38811eb2717a099c
Nicola <ciao@ciao>
2019-12-30 20:32:41 +0100

    Configures the integration-test to starts two clients, one in a separate process

--- client/src/main/java/MainClassWQClient.java
+++ client/src/main/java/MainClassWQClient.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class MainClassWQClient {
     public static void main(String[] args) {
         System.out.println("Starting Client");
+        JFrame frame = new JFrame("My First GUI");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        JButton button = new JButton("Press");
+        button.setPreferredSize(new Dimension(100, 100));
+        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        frame.setVisible(true);
+        System.out.println("CIAONE");
     }
 }
